ca234f9aaf8d5be8854dd43404f9fe73a6a5b70e
Diego Casa <diegocasa@MacBook-Pro-de-Tecsup-3.local>
2017-05-08 16:07:40 -0500

    Initial Commit

---
+++ SoundBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundBoard
+//
+//  Created by Diego Casa on 8/05/17.
+//  Copyright © 2017 Diego Casa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
